6e73a4a9bdea722d12632bf535ca8b61fed02aaf
Michael R.F <mrflowers22@uwalumni.com>
2019-06-26 12:31:34 -0700

    trying to checkout master

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -45,6 +45,22 @@ class GameAITests: XCTestCase {
         XCTAssertFalse(game(board: board, isWonBy: .x))
     }
     
+    func testWinCheckingVertical3() {
+        var board = GameBoard()
+        /*
+         - o x
+         - o x
+         - - x
+         */
+        try! board.place(mark: .x, on: (0, 2))
+        try! board.place(mark: .o, on: (0, 1))
+        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .o, on: (1, 1))
+        try! board.place(mark: .x, on: (2, 2))
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+    }
+    
     func testWinCheckingHorizontal1() {
         var board = GameBoard()
         /*
@@ -52,6 +68,14 @@ class GameAITests: XCTestCase {
          x x x
          o - -
          */
+        
+        try! board.place(mark: .x, on: (0, 1))
+        try! board.place(mark: .o, on: (1, 0))
+        try! board.place(mark: .x, on: (1, 1))
+        try! board.place(mark: .o, on: (2, 0))
+        try! board.place(mark: .x, on: (1, 2))
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
     }
     
     func testWinCheckingHorizontal2() {
