eaf160d12a58da322d6775afabb893ebe87af6a4
Michael R.F <mrflowers22@uwalumni.com>
2019-06-26 12:34:49 -0700

    implemented check horizontal 3

--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -69,11 +69,16 @@ class GameAITests: XCTestCase {
          o - -
          */
         
+        //X goes first
         try! board.place(mark: .x, on: (0, 1))
         try! board.place(mark: .o, on: (1, 0))
         try! board.place(mark: .x, on: (1, 1))
-        try! board.place(mark: .o, on: (2, 0))
-        try! board.place(mark: .x, on: (1, 2))
+        try! board.place(mark: .o, on: (0, 2))
+        try! board.place(mark: .x, on: (2, 1))
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+        
+        
         XCTAssertTrue(game(board: board, isWonBy: .x))
         XCTAssertFalse(game(board: board, isWonBy: .o))
     }
@@ -85,6 +90,8 @@ class GameAITests: XCTestCase {
          - x -
          o o o
          */
+        
+        
     }
     
     func testWinCheckingDiagonal1() {
